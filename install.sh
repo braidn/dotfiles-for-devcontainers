3ff28c9871b8523024a4f17578046a5392e3b86d
Braden Douglass <braden.douglass@gmail.com>
2022-07-14 13:49:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-# Install spaceship prompt
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git $HOME/.oh-my-zsh/custom/themes/spaceship-prompt --depth=1
-
 # Install ZSH syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
